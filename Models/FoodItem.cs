5bd9ef992153b7d70b33a2c1d519feb9f184c854
Aither <cheese736@hotmail.com>
2023-06-21 14:40:46 +0800

    add Model: FoodItem

--- Models/FoodItem.cs
+++ Models/FoodItem.cs
@@ -5,8 +5,13 @@ using System.Threading.Tasks;
 
 namespace fungoodMVC.Models
 {
-    public class FoodItem
-    {
-        
-    }
+	public class FoodItem
+	{
+		public int Id { get; set; }
+
+		public string Name { get; set; } = string.Empty;
+		public int Price { get; set; }
+		public int CategoryId { get; set; }
+
+	}
 }
--- Services/IOrderService.cs
+++ Services/IOrderService.cs
@@ -2,11 +2,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using fungoodMVC.Models;
 
 namespace fungoodMVC.Services
 {
-    public interface IOrderService
-    {
-        
-    }
+	public interface IOrderService
+	{
+		/*提供點餐、查看餐點、結帳*/
+		void SendOrder(List<FoodItem> order, int tableNumber);
+		List<FoodItem> CheckOrder(int tableNumber);
+
+		int Pay(int tableNumber);
+	}
 }
