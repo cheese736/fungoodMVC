60cd3528bd1ac153b5892c2a0ab385abec678149
Aither <cheese736@hotmail.com>
2023-07-15 21:50:43 +0800

    add OrderNumber to Table.cs

---
+++ Migrations/20230715134733_AddOrderNumberToTable.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace fungoodMVC.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddOrderNumberToTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "OrderNumer",
+                table: "tables",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "tables",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "OrderNumer",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "tables",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "OrderNumer",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "tables",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "OrderNumer",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "tables",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "OrderNumer",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "tables",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "OrderNumer",
+                value: null);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OrderNumer",
+                table: "tables");
+        }
+    }
+}
