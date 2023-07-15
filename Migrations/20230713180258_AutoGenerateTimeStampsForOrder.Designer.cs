d55798cd5423550cb7e97babf3be6517ed0ab653
Aither <cheese736@hotmail.com>
2023-07-15 21:39:33 +0800

    Add auto-generate timestamps for Order.cs

--- Migrations/20230713180258_AutoGenerateTimeStampsForOrder.Designer.cs
+++ Migrations/20230713180258_AutoGenerateTimeStampsForOrder.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using fungoodMVC.Data;
 
@@ -11,9 +12,11 @@ using fungoodMVC.Data;
 namespace fungoodMVC.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20230713180258_AutoGenerateTimeStampsForOrder")]
+    partial class AutoGenerateTimeStampsForOrder
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20230713180258_AutoGenerateTimeStampsForOrder.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace fungoodMVC.Migrations
+{
+    /// <inheritdoc />
+    public partial class AutoGenerateTimeStampsForOrder : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_orders_tables_TableId",
+                table: "orders");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TableId",
+                table: "orders",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "LastUpdated",
+                table: "orders",
+                type: "datetime2",
+                rowVersion: true,
+                nullable: false,
+                defaultValueSql: "GETDATE()",
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldRowVersion: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Inserted",
+                table: "orders",
+                type: "datetime2",
+                nullable: false,
+                defaultValueSql: "GETDATE()",
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_orders_tables_TableId",
+                table: "orders",
+                column: "TableId",
+                principalTable: "tables",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_orders_tables_TableId",
+                table: "orders");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TableId",
+                table: "orders",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "LastUpdated",
+                table: "orders",
+                type: "datetime2",
+                rowVersion: true,
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldRowVersion: true,
+                oldDefaultValueSql: "GETDATE()");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Inserted",
+                table: "orders",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValueSql: "GETDATE()");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_orders_tables_TableId",
+                table: "orders",
+                column: "TableId",
+                principalTable: "tables",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
