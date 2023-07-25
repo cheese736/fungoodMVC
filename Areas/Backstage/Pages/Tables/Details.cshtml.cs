37432f2db3e8df4098b3f8ac5152e4cacfd211c3
Aither <cheese736@hotmail.com>
2023-07-26 02:46:22 +0800

    add checkout modal

--- Areas/Backstage/Pages/Tables/Details.cshtml.cs
+++ Areas/Backstage/Pages/Tables/Details.cshtml.cs
@@ -11,14 +11,18 @@ namespace fungoodMVC.Areas.Backstage.Pages.Tables
 	public class Details : PageModel
 	{
 		private readonly DataContext _context;
-		public List<Models.Order> Orders { get; set; } = new List<Models.Order>();
+        private readonly IdentityDbContext _identityDbContext;
+
+        public List<Models.Order> Orders { get; set; } = new List<Models.Order>();
 		public int TableId { get; set; }
 		public int OrderNumber { get; set; }
+		public string? UserName { get; set; }
 
-		public Details(DataContext context)
+        public Details(DataContext context, IdentityDbContext identityDbContext)
 		{
 			_context = context;
-		}
+            _identityDbContext = identityDbContext;
+        }
 
 		public async Task<IActionResult> OnGetAsync(int orderNumber)
 		{
@@ -27,8 +31,15 @@ namespace fungoodMVC.Areas.Backstage.Pages.Tables
 			.Include(o => o.Table)
 			.Where(o => o.OrderNumber == orderNumber)
 			.ToListAsync();
+			var userId = orders[0].UserId;
+			if (userId is not null)
+			{
+				var user = await _identityDbContext.Users.FirstOrDefaultAsync(u => u.Id == userId);
+				UserName = user?.UserName;
+			}
 
-			if (orders == null)
+
+            if (orders == null)
 			{
 				return NotFound();
 			}
