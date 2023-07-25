f792b77f26811322232543d73a1d68fa0fe273ca
Aither <cheese736@hotmail.com>
2023-07-25 23:09:04 +0800

    Add Bonus and CumulativeConsumption to users

---
+++ Migrations/Identity/20230725143657_AddBonusToUsers.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace fungoodMVC.Migrations.Identity
+{
+    /// <inheritdoc />
+    public partial class AddBonusToUsers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Bonus",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "CumulativeConsumption",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Bonus",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "CumulativeConsumption",
+                table: "AspNetUsers");
+        }
+    }
+}
