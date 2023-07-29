e8357fb329fe1010f1bc8e0a050c38b66b6fe33a
Aither <cheese736@hotmail.com>
2023-07-29 18:09:20 +0800

    Order.Table is not nullable now

--- Models/Order.cs
+++ Models/Order.cs
@@ -11,7 +11,7 @@ namespace fungoodMVC.Models
 		public int Id { get; set; }
 		public int OrderNumber { get; set; }
 		public FoodItem FoodItem { get; set; } = new FoodItem();
-		public Table? Table { get; set; }
+		public Table Table { get; set; } = new Table();
 		public Spiciness? Spiciness { get; set; }
 		public string? UserId { get; set; }
 		public bool Check { get; set; }
