d3cc1da2af71dbf72089a03a6a4de0ad8b920092
Aither <cheese736@hotmail.com>
2023-08-01 02:57:33 +0800

    add LogModelStateErrors()

--- Helper/Dumper.cs
+++ Helper/Dumper.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
 namespace fungoodMVC.Helper
 {
 	public static class Dumper
@@ -6,6 +8,14 @@ namespace fungoodMVC.Helper
 		{
 			var dumper = ObjectDumper.Dump(obj);
 			System.Console.WriteLine(dumper);
+		}
+
+		public static void LogModelStateErrors(ModelStateDictionary modelState)
+		{
+			var errors = modelState.Select(x => x.Value.Errors)
+			.Where(y => y.Count > 0)
+			.ToList();
+			errors.ForEach(e => Dumper.print(e));
 		}
 	}
 }
